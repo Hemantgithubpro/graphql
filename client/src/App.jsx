e579524c5859a4a004e792d82ea5c4045506bef4
Hemant <hemantjha2005@gmail.com>
2025-03-18 00:59:47 +0530

    made a small project with graphql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import {ApolloClient} from '@apollo/client'
+// import {ApolloClient} from '@apollo/client'
 
-const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
-  cache: new InMemoryCache()
-});
+// const client = new ApolloClient({
+//   uri: 'http://localhost:8000/graphql',
+//   cache: new InMemoryCache()
+// });
 
 // const query = `
 //   query{
@@ -19,12 +18,45 @@ const client = new ApolloClient({
 //   }
 // `
 
+import {gql, useQuery} from '@apollo/client'
+
+const query = gql`
+  query GetTodosOfUser{
+    getTodos{
+      id
+      title
+      completed
+      user{
+        id
+        name
+      }
+    }
+  }
+`
+
 function App() {
-  const [count, setCount] = useState(0)
+  const {data, loading} = useQuery(query)
+
+  if(loading) return <h1>Loading...</h1>
+
+  console.log(data)
 
   return (
     <>
-      
+      {/* {JSON.stringify(data)}; */}
+      <table>
+        <tbody>
+          {
+            data.getTodos.map(todo => 
+              <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.title}</td>
+                <td>{todo.completed ? 'Completed' : 'Not Completed'}</td>
+                <td>{todo.user.name}</td>
+              </tr>)
+          }
+        </tbody>
+      </table>
     </>
   )
 }
